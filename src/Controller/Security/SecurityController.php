26c6f46cdbf2a96e252ebea6fbb9dcadbc7a6a53
Jourani <mjourani.orca@gmail.com>
2019-03-25 18:01:26 +0000

    Security

---
+++ src/Controller/Security/SecurityController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Joe
+ * Date: 25/03/2019
+ * Time: 16:06
+ */
+
+namespace App\Controller\Security;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $lastUserName = $authenticationUtils->getLastUsername();
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        return $this->render('Security/login.html.twig',array(
+            'last_username'=> $lastUserName,
+            'error'=> $error
+        ));
+    }
+}
